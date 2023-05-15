c1d3bb9d3bc5e8b07aee611ec4422824d530faa1
Daniel Bar Lev <Danielbl2010@gmail.com>
2023-05-16 02:19:44 +0300

    Add a class for bird constants

--- src/constants/constants.py
+++ src/constants/constants.py
@@ -1,6 +1,25 @@
 from enum import Enum
+import pygame
+import os
 
-class Constants(Enum):
+class Global(Enum):
 
+    # window
     WIN_WIDTH = 600
     WIN_HEIGHT = 800
+
+    PIPE_IMAGE = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","media/pipe.png")))
+    BASE_IMAGE = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "media/base.png")))
+    BACKGROUND_IMAGE = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "media/bg.png")))
+
+class Bird(Enum):
+
+    # fixed attributes
+    MAX_ROTATION = 25
+    ROTATION_VELOCITY = 20
+    ANIMATION_TIME = 5
+
+    # media load
+    Bird_IMAGE_1 = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "media/bird1.png")))
+    Bird_IMAGE_2 = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "media/bird2.png")))
+    Bird_IMAGE_3 = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "media/bird3.png")))
